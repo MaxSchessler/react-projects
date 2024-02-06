d8152880a3bea8b1ee1ec176be52611c4766cbc0
Max Schessler <schessler.max@gmail.com>
2024-02-05 23:38:10 -0600

    added accordian

---
+++ accordian/src/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: "1",
+        question: "what are accordion components?",
+        answer: "Accordian componets are user interface elements used for orgnaizing data"
+    }, 
+    {
+        id: "2",
+        question: "What are they used for? ",
+        answer: "Theyt are commonly employed in various context, including FAQs, and quizes"
+    }, 
+    {
+        id: "3",
+        question: "Accordian as a musical instrument?",
+        answer: "The accordian is a musical instrument with a keyboard and bellows"
+    },
+    {
+        id: "4",
+        question: "Who is my Dad?",
+        answer: "N/A"
+    }
+]
+
+export default data;
---
+++ accordian/src/index.jsx
@@ -0,0 +1,69 @@
+// import statements
+import React, { useState } from "react";
+import data from "./data";
+
+// single selection
+// multiple selections
+export default function Accordian() {
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
+
+  function handleSingleSelection(currentID) {
+    // receive the current id of the queswtion
+    // if selected hook is dataItem.id, hide text by setting selected id to 0
+    setSelected(selected === currentID ? "0" : currentID);
+  }
+
+  function handleMultiSelection(currentID) {
+    let copyOfMultiple = [...multiple];
+    const findIndexOfCurrentID = copyOfMultiple.indexOf(currentID);
+
+    if (findIndexOfCurrentID === -1) copyOfMultiple.push(currentID);
+    else copyOfMultiple.splice(findIndexOfCurrentID, 1);
+
+    setMultiple(copyOfMultiple);
+  }
+
+  return (
+    <div className="wrapper">
+      <h2 className="title">Accordian FAQ!</h2>
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div
+              onClick={
+                enableMultiSelection
+                  ? () => handleMultiSelection(dataItem.id)
+                  : () => handleSingleSelection(dataItem.id)
+              }
+              className={"item"}
+              key={dataItem.id}
+            >
+              <h2 className="question">{dataItem.question}</h2>
+
+              {
+                // if enableMultiSelection is true, check multiple
+                enableMultiSelection ? multiple.indexOf(dataItem.id) !== -1 && <div className="content">{dataItem.answer}</div> :
+                // if selected item is dataItem.id show data.answer below data question
+                selected === dataItem.id ? (
+                  <div className="content">{dataItem.answer}</div>
+                ) : (
+                  <div className="content">+</div>
+                )
+              }
+            </div>
+          ))
+        ) : (
+          <div>Data not found! </div>
+        )}
+      </div>
+      <h2>
+        <button 
+            className="multiSelection" 
+            onClick={() => enableMultiSelection ? setEnableMultiSelection(false) : setEnableMultiSelection(true)}
+        >Enable Multiselection</button>
+      </h2>
+    </div>
+  );
+}
