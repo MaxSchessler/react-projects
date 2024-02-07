f85fdcd230c7e63a5762b7a9b51259dd4ae3b2e2
Max Schessler <schessler.max@gmail.com>
2024-02-07 10:28:33 -0600

    changes to qr-code

---
+++ qr-code-generator/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>QR Code Generator</h1>
+      <div>
+        <input type="text" name=""></input>
+      </div>
+      <button>Generate</button>
+    </div>
+  );
+}
+
+export default App;
