1d19647c7e742d9b144a843e885e461cb93d436d
Max Schessler <schessler.max@gmail.com>
2024-08-14 08:06:26 -0500

    Added conditional messsages to LoginComponent.jsx

--- todoapplication/src/components/todo/LoginComponent.jsx
+++ todoapplication/src/components/todo/LoginComponent.jsx
@@ -4,28 +4,48 @@ const LoginComponent = () => {
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [showErrorMessage, setShowErrorMessage] = useState(false);
+
+    function handleSubmit() {
+        console.log(`Username: ${username}\nPassword: ${password}`);
+
+        if (username === "maxschessler" && password === "password") {
+            // show success div
+            setShowSuccessMessage(true);
+            setShowErrorMessage(false);
+        } else {
+            // show error div
+            setShowSuccessMessage(false);
+            setShowErrorMessage(true);
+        }
+    }
 
     return (
         <div className="login">
+            {showSuccessMessage ? <div className="successMessage">Authenticated Successfully</div> : null}
+            {showErrorMessage ? <div className="loginContainer">Authentication Failed.</div>: null}
             <div className="login-form">
                 <div>
                     <label htmlFor="username" className="login-label">Username</label>
                     <input type="text" name="username" className="login-input"
-                           value={username} onChange={(e) => setUsername(e.target.value)} />
+                           value={username} onChange={(e) => setUsername(e.target.value)}/>
                 </div>
                 <div>
 
                     <label htmlFor="password" className="login-label">Password</label>
                     <input type="password" name="password" className="login-input"
-                            value={password} onChange={(e) => setPassword(e.target.value)} />
+                           value={password} onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 <div>
                     <button type="submit" className="login-button" onClick={
                         () => {
-                            console.log(`Username ${username}\nPassword ${password}`);
+                            handleSubmit();
                         }
-                    }>Login</button>
+                    }>Login
+                    </button>
                 </div>
+
             </div>
         </div>
     )
