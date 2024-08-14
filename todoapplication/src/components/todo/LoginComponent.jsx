4e414b4268d9528ddb05469e10d0bbae6c17d362
Max Schessler <schessler.max@gmail.com>
2024-08-13 21:26:19 -0500

    Updated LoginComponent.jsx

--- todoapplication/src/components/todo/LoginComponent.jsx
+++ todoapplication/src/components/todo/LoginComponent.jsx
@@ -1,19 +1,30 @@
 import React, { useState, useEffect } from "react";
 
 const LoginComponent = () => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         <div className="login">
             <div className="login-form">
                 <div>
                     <label htmlFor="username" className="login-label">Username</label>
-                    <input type="text" name="username" className="login-input"/>
+                    <input type="text" name="username" className="login-input"
+                           value={username} onChange={(e) => setUsername(e.target.value)} />
                 </div>
                 <div>
-                    <label htmlFor="password" className="login-label">Username</label>
-                    <input type="password" name="password" className="login-input"/>
+
+                    <label htmlFor="password" className="login-label">Password</label>
+                    <input type="password" name="password" className="login-input"
+                            value={password} onChange={(e) => setPassword(e.target.value)} />
                 </div>
                 <div>
-                    <button type="submit" className="login-button">Login</button>
+                    <button type="submit" className="login-button" onClick={
+                        () => {
+                            console.log(`Username ${username}\nPassword ${password}`);
+                        }
+                    }>Login</button>
                 </div>
             </div>
         </div>
--- todoapplication/src/components/todo/TodoApp.jsx
+++ todoapplication/src/components/todo/TodoApp.jsx
@@ -1,6 +1,7 @@
 
 import React, { useState, useEffect } from "react";
 import LoginComponent from "./LoginComponent";
+import "../../styles/TodoApp.css"
 
 const TodoApp = () => {
 
